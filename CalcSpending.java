0851e1158dd4f281a54305ff60c8c1f839d8cfb2
Oliver <oliverkeh@gmail.com>
2016-10-12 17:21:09 -0400

    Adding Gmail API functionality with Gradle

--- CalcSpending.java
+++ CalcSpending.java
@@ -4,7 +4,7 @@ import java.util.Date;
 import helperfunctions.*;
 
 public class CalcSpending {
-	/*public static void printinfo(URL url) throws IOException {
+	public static void printinfo(URL url) throws IOException {
 		URLConnection c = url.openConnection();
 		c.connect();
 
@@ -18,26 +18,28 @@ public class CalcSpending {
 		if (c instanceof HttpURLConnection) {
 			HttpURLConnection h = (HttpURLConnection) c;
 			System.out.println("  Request Method: " + h.getRequestMethod());
-			System.out.println("  Response Message: " + \
-			h.getResponseMessage());
+			System.out.println("  Response Message: " + 
+								  h.getResponseMessage());
 			System.out.println("  Response Code: " + h.getResponseCode());
 		}
 
-	}*/
+	}
 
 	public static void main(String[] args) {
-		/*try { printinfo(new URL(args[0])); }
+		try { printinfo(new URL(args[0])); }
 		catch (Exception e) {
 			System.err.println(e);
 			System.err.println("Usage: java GetURLInfo <url>");
-		} */
+		} 
+
+
 
 		/* Prints the line of text that contains the correct banking
 		   information */
-		try {
+		//try {
 			if (args.length != 2)
 				throw new IllegalArgumentException("Wrong number of args");
-			helperfunctions.GrepReader in = new helperfunctions.GrepReader(new FileReader(args[1]), args[0]);
+			helperfunctions.GrepReader in = new helperfunctions.GrepReader(new FileReader(), "default");
 			String line;
 
 			while ((line = in.readLine()) != null) {
@@ -45,11 +47,11 @@ public class CalcSpending {
 			}
 
 			in.close();
-		}
+		//}
 
-		catch (Exception e) {
+		/*catch (Exception e) {
 			System.err.println(e);
 			System.out.println("Usage: java CalcSpending" + " <pattern> <file>");
-		}
+		}*/
 	}
 }
